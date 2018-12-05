3072368011e413b3c14be38b0bf169338c14d91b
BillydzyChan <billydzy@gmail.com>
2018-12-05 19:41:16 +0800

    run

---
+++ build/CMakeFiles/vegnews.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vegnews.dir/stringbad.cpp.obj"
+  "CMakeFiles/vegnews.dir/vegnews.cpp.obj"
+  "vegnews.pdb"
+  "vegnews.exe"
+  "vegnews.exe.manifest"
+  "libvegnews.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vegnews.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
